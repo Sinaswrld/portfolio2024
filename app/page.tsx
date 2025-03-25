191f97dc7ba901c16280e38af0ba285607e299ba
Sinaswrld <s2vahidi@uwaterloo.ca>
2025-03-25 11:43:38 -0700

    intro background colours change

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Contact from '@/components/contact';
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center px-4 bg-transparent">
+    <main className="flex flex-col items-center px-4">
       <Intro />
       <SectionDivider />
       <About />
