a9ec63e5452a52f909ab656b9e10b0c6afe5b257
Sinaswrld <s2vahidi@uwaterloo.ca>
2025-03-25 12:32:33 -0700

    change footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -3,9 +3,7 @@ import React from 'react';
 export default function Footer() {
   return (
     <footer className="mb-10 px-4 text-center text-gray-500">
-      <small className="mb-2 block text-cs">
-        &copy; 2025 Sina. All rights reserved.
-      </small>
+      <small className="mb-2 block text-cs">&copy; 2025 Sina</small>
     </footer>
   );
 }
