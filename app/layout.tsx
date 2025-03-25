a8f7f8b995579964c9c3fa7dbd90b6755d576829
Sinaswrld <s2vahidi@uwaterloo.ca>
2025-03-25 11:50:24 -0700

    change blur to px

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,8 +24,8 @@ export default function RootLayout({
       <body
         className={`${inter.className} 
       bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}>
-        <div className="bg-[#fbe2e3] absolute top-[-2%] -z-10 right-[10%] h-[10%] w-[50%] rounded-full blur-[10rem] sm:w-[40%] dark:bg-[#946263]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1%] -z-10 left-[10%] h-[10%] w-[50%] rounded-full blur-[10rem] sm:w-[40%] dark:bg-[#676394]"></div>
+        <div className="bg-[#fbe2e3] absolute top-[-2%] -z-10 right-[10%] h-[10%] w-[50%] rounded-full blur-[200px] sm:w-[40%] dark:bg-[#946263]"></div>
+        <div className="bg-[#dbd7fb] absolute top-[-1%] -z-10 left-[10%] h-[10%] w-[50%] rounded-full blur-[200px] sm:w-[40%] dark:bg-[#676394]"></div>
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
             <Header />
