52fd0fc2f622211250fcc3aae72ea4ad61b5dd0f
Sinaswrld <s2vahidi@uwaterloo.ca>
2025-04-04 17:24:22 -0400

    added new project and changed resume and skills

--- components/project.tsx
+++ components/project.tsx
@@ -1,11 +1,11 @@
-'use client';
+"use client";
 
-import { useRef } from 'react';
-import { projectsData } from '@/lib/data';
-import Image from 'next/image';
-import { useScroll, useTransform } from 'framer-motion';
-import { motion } from 'framer-motion';
-import Link from 'next/link';
+import { useRef } from "react";
+import { projectsData } from "@/lib/data";
+import Image from "next/image";
+import { useScroll, useTransform } from "framer-motion";
+import { motion } from "framer-motion";
+import Link from "next/link";
 
 type ProjectProps = (typeof projectsData)[number];
 
@@ -19,7 +19,7 @@ export default function Project({
   const ref = useRef<HTMLDivElement>(null);
   const { scrollYProgress } = useScroll({
     target: ref,
-    offset: ['0 1', '1.33 1'],
+    offset: ["0 1", "1.33 1"],
   });
   const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.5, 1]);
 
@@ -31,8 +31,8 @@ export default function Project({
         opacity: scrollYProgress,
       }}
       className="group mb-3 sm:mb-8 last:mb-0">
-      <section className="relative bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden rounded-lg sm:pr-8 sm:h-fit sm:group-even:pl-10 hover:bg-gray-200 transition dark:bg-white/10 dark:hover:bg-white/20">
-        <div className="pt-4 pb-7 px-5 sm:pl-8 sm:pr-2 sm:pt-10 sm:max-w-[55%] sm:group-even:max-w-[60%]  flex flex-col h-full sm:group-even:ml-[16rem]">
+      <section className="relative bg-gray-100 max-w-[42rem] border border-black/5 overflow-hidden rounded-lg sm:h-fit hover:bg-gray-200 transition dark:bg-white/10 dark:hover:bg-white/20">
+        <div className="py-7 px-5 sm:max-w-[52%] sm:group-odd:pr-0 sm:group-even:pl-0 flex flex-col h-full sm:group-even:ml-[48%]">
           <h3 className="text-2xl font-semibold">{title}</h3>
           <p className="mt-2 leading-relaxed text-gray-700 dark:text-white/70">
             {description}
@@ -44,7 +44,7 @@ export default function Project({
               View
             </Link>
           ) : (
-            ''
+            ""
           )}
           <ul className="flex  flex-wrap mt-4 gap-2 sm:mt-auto">
             {tags.map((tag, index) => (
@@ -58,7 +58,7 @@ export default function Project({
         </div>
 
         <Image
-          className="absolute md:top-[31%] top-[33%] -right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:-right-[initial] group-even:-left-40 group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 transition hidden sm:block "
+          className="absolute top-[29%] -right-48 md:-right-40 w-[28.25rem] rounded-t-lg shadow-2xl group-even:-right-[initial] group-even:-left-48 md:group-even:-left-40 group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 transition hidden sm:block "
           src={imageUrl}
           alt="Project I worked on"
           quality={95}
